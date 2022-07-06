c36920dd9c293a1dc27037960bc17bb898d53200
zaaiiin <zaainnaseem@gmail.com>
2022-07-06 10:59:18 +0800

    adding varables

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const first = document.getElementById('fname');
+const last = document.getElementById('lname');
+const email = document.getElementById('email');
+const pword = document.getElementById('pword');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+});
