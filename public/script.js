6db3e6e8bba7eea766a2630ab94fd818202bd359
zaaiiin <zaainnaseem@gmail.com>
2022-07-15 13:03:07 +0800

    regex

--- public/script.js
+++ public/script.js
@@ -48,9 +48,7 @@ function removeError(field){
     formControl.classList.remove('error');
 }
 
-//check this part
-
 function isValid(email){
-  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  return re.test(String(email).toLowerCase());
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
